401b09d3afeb0ff531cf86695b4fa60ebc5a9582
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-26 05:26:18 -0500

    added the it block and comments explaining how it is executed. also added a description inside of the it block

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,12 @@
 const assert = require('chai').assert;
 const app = require('../app');
 
+//start with a describe function which encapsulates a section
+describe('App', () => {
+    
+//inside of this describe there is an it function to test a particular feature
+    it('App should return hello', () => {
+
+
+    })
+})
